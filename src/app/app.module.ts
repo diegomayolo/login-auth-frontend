9029cd1facaf040d99ad901e0f17542e120f2561
Diego Luis Mayolo <diego.mayolo@outlook.com>
2024-10-27 13:03:59 -0300

    FEATURE: fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
+import { PrimaryInputComponent } from './components/primary-input/primary-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,9 +1,13 @@
 import {Component, Input} from '@angular/core';
+import {ɵEmptyOutletComponent} from "@angular/router";
 
 @Component({
   selector: 'app-default-login-layout',
   templateUrl: './default-login-layout.component.html',
   standalone: true,
+  imports: [
+    ɵEmptyOutletComponent
+  ],
   styleUrls: ['./default-login-layout.component.scss']
 })
 export class DefaultLoginLayoutComponent {
